10ad52dd39a0a680f205302a829d735ea402aeac
estas1o <93281153+estas1o@users.noreply.github.com>
2025-04-01 18:53:43 +0300

    create and drop table

--- admin.sql
+++ admin.sql
@@ -12,3 +12,15 @@ add column col_3 text
 
 alter table test
 rename column col_3 to col_4;
+
+alter table test
+drop col_4;
+
+insert into test (col_1, col_2)
+values ('text', 1);
+
+create table text_copy	as
+select * from test;
+
+drop table text_copy;
+drop table test;
